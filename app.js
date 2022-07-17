186ddd8173126413365a15c67d527828e2bded7b
KesterDaniel <kesterdan17@gmail.com>
2022-07-17 18:39:11 +0100

    added mongoose

--- app.js
+++ app.js
@@ -1,39 +1,64 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 const port = 3000 || process.env.PORT 
- 
+
+mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', {
+    useUnifiedTopology: true
+})
+
+const CampgroundSchema = mongoose.Schema({
+    name: String,
+    image: String
+})
+
+const Campground = mongoose.model("campground", CampgroundSchema)
+
+// async function TestCamp(){
+//     try {
+//         const MyCamp = await Campground.create({
+//             name: "Salmona creek",
+//             image: "https://pixabay.com/get/ga9b3e521a9e485904f749a384cfccff35a58c3e992a624cceda5140b9d03f5386c6c4b3abc1f8dcb168b1f9c24ab9d4a_340.jpg",
+//         })
+//         console.log("Newly Created Campground: ")
+//         console.log(MyCamp)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+// TestCamp()
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
-const campgrounds = [
-    {Name: "Nyt Joy", image: "https://pixabay.com/get/g036b71c651efd8e26c5e9a03f643c491868661cb0a4b3eba5bb74c01d3c110efacb473a28e1bb688cbc1273a3800ebdd_340.jpg"},
-    {Name: "Salmona creek", image: "https://pixabay.com/get/g605494ed882c0664720135b32e645ce8e73fee1e94f9f7d2a79fada4d7f6938e8296cff9c996c5625d0f02e20919dd9b_340.jpg"},
-    {Name: "Shadow lights", image: "https://pixabay.com/get/g8f6689264482d1631bb1211faea41a65b2f53eca22cd76fa8082e63caf5e4ad424b38a507999689abf36eb0cc27f796f_340.jpg"},
-    {Name: "Nyt Joy", image: "https://pixabay.com/get/g036b71c651efd8e26c5e9a03f643c491868661cb0a4b3eba5bb74c01d3c110efacb473a28e1bb688cbc1273a3800ebdd_340.jpg"},
-    {Name: "Salmona creek", image: "https://pixabay.com/get/g605494ed882c0664720135b32e645ce8e73fee1e94f9f7d2a79fada4d7f6938e8296cff9c996c5625d0f02e20919dd9b_340.jpg"},
-    {Name: "Shadow lights", image: "https://pixabay.com/get/g8f6689264482d1631bb1211faea41a65b2f53eca22cd76fa8082e63caf5e4ad424b38a507999689abf36eb0cc27f796f_340.jpg"},
-    {Name: "Nyt Joy", image: "https://pixabay.com/get/g036b71c651efd8e26c5e9a03f643c491868661cb0a4b3eba5bb74c01d3c110efacb473a28e1bb688cbc1273a3800ebdd_340.jpg"},
-    {Name: "Salmona creek", image: "https://pixabay.com/get/g605494ed882c0664720135b32e645ce8e73fee1e94f9f7d2a79fada4d7f6938e8296cff9c996c5625d0f02e20919dd9b_340.jpg"},
-    {Name: "Shadow lights", image: "https://pixabay.com/get/g8f6689264482d1631bb1211faea41a65b2f53eca22cd76fa8082e63caf5e4ad424b38a507999689abf36eb0cc27f796f_340.jpg"},
-]
  
 app.get("/", (req, res)=>{
     res.render("landing")
 })
 
-app.get("/campgrounds", (req, res)=>{
-    res.render("campgrounds", { campgrounds })
+app.get("/campgrounds", async(req, res)=>{
+    try {
+        const allCampgrounds = await Campground.find({})
+        res.render("campgrounds", { campgrounds: allCampgrounds })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-app.post("/campgrounds", (req, res)=>{
-    const Name = req.body.Name
+app.post("/campgrounds", async(req, res)=>{
+    const name = req.body.name
     const image = req.body.image
-    const newCampground = { Name, image}
-    campgrounds.push(newCampground)
-    res.redirect("/campgrounds")
+    const newCampground = { name, image}
+    try {
+        await Campground.create(newCampground)
+        console.log("CampGround successfully created")
+        res.redirect("/campgrounds")
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.get("/campgrounds/new", (req, res)=>{
