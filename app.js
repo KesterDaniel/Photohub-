074799f2d5238e52c670f15a92eb2fb1b872e31f
KesterDaniel <kesterdan17@gmail.com>
2022-07-28 15:29:19 +0100

    creating comments and linking them with campgrounds

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const mongoose = require("mongoose")
 const port = 3000 || process.env.PORT 
 
 const Campground = require("./models/campgrounds")
+const Comment = require("./models/comments")
 const seedDb = require("./seeds")
 
 mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', {
     useUnifiedTopology: true
 })
 
-seedDb() 
-
-
 
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+
+seedDb() 
 
 
 //ROOT ROUTE
@@ -62,12 +62,41 @@ app.get("/campgrounds/:id", async(req, res)=>{
     try {
         const chosenCamp = await Campground.findById(CampId)
         await chosenCamp.populate("comments")
-        console.log(chosenCamp)
         res.render("show", {campground: chosenCamp})
     } catch (error) {
         console.log(error)
     }
 })
+
+//==============
+//COMMENTS ROUTE
+//==============
+
+app.get("/campgrounds/:id/comments/new", async(req, res)=>{
+    try {
+        const campground = await Campground.findById(req.params.id)
+        res.render("newComment", { campground })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post("/campgrounds/:id/comments", async(req, res)=>{
+    const newComment = req.body.comment
+    const campId = req.params.id
+    try {
+       const MyComment = await Comment.create(newComment)
+       const camp = await Campground.findById(campId)
+       await camp.comments.push(MyComment)
+       await camp.save()
+       res.redirect(`/campgrounds/${campId}`)
+       console.log("added comment")
+    } catch (error) {
+        res.redirect("/campgrounds")
+        console.log(error)
+    }
+})
+
 
 app.listen(port, ()=>{
     console.log("Yelp is up")
