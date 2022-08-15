b64ea14bd202ebcd145c14779c84d675c74f914c
KesterDaniel <kesterdan17@gmail.com>
2022-08-16 00:22:23 +0100

    added image upload feature

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,6 +3,28 @@ const router = new express.Router()
 const Campground = require("../models/campgrounds")
 const middleware = require("../middleware")
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'do1xoszkp', 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 //INDEX ROUTE
 router.get("/", async(req, res)=>{
     try {
@@ -14,24 +36,23 @@ router.get("/", async(req, res)=>{
 })
 
 //CREATE ROUTE
-router.post("/", middleware.isLoggedIn, async(req, res)=>{
-    const name = req.body.name
-    const image = req.body.image
-    const description = req.body.description
-    const Author = {
-        id: req.user._id,
-        username: req.user.username
-    }
-    const price = req.body.price
-    const newCampground = { name, image, description, Author, price}
+router.post("/", middleware.isLoggedIn, upload.single("image"), async(req, res)=>{
     try {
-        await Campground.create(newCampground)
-        console.log("CampGround successfully created")
-        req.flash("success", "CampGround successfully created")
-        res.redirect("/campgrounds")
+        const result = await cloudinary.uploader.upload(req.file.path) 
+        // add cloudinary url for the image to the campground object under image property
+        req.body.campground.image = result.secure_url;
+        // add author to campground
+        req.body.campground.Author = {
+          id: req.user._id,
+          username: req.user.username
+        }
+        const campground = await Campground.create(req.body.campground)
+        res.redirect(`/campgrounds/${campground.id}`)
     } catch (error) {
-        console.log(error)
+        req.flash("error", error.message)
+        res.redirect("back")
     }
+        
 })
 
 //NEW ROUTE
